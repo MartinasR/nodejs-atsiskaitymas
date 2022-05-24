e0eb0020b50de4f22f4a12689907f9e0936a9d08
MartinasR <martinasrudis@gmail.com>
2022-05-24 09:21:03 +0300

    gitignore

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import 'dotenv/config';
+import cookieParser from 'cookie-parser';
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import userRouter from './routes/api/user.js';
+import blogRouter from './routes/api/blog.js'
 
 
 const app = express();
@@ -10,10 +14,17 @@ const corsOptions = {
     optionsSuccessStatus: 200
   };
 
+  app.set('views', path.join('views'));
+app.set('view engine', 'ejs')
+
   app.use(cors(corsOptions)); 
 app.use(express.json());
 app.use(express.urlencoded({ 
     extended: false
 }));
+
+app.use(express.static('public'))
+app.use('/api/user', userRouter)
+app.use('/api/blog', blogRouter)
 
   app.listen(5000, () => console.log(`Serveris veikia ant ${PORT} porto.`));
