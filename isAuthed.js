81e6f19cfa6a94b70b042bd8810009d1dc105f3a
MartinasR <martinasrudis@gmail.com>
2022-05-24 15:22:44 +0300

    is Authed jwt token

--- isAuthed.js
+++ isAuthed.js
@@ -3,7 +3,7 @@ import jwt from 'jsonwebtoken';
 export default async function isAuthed(req) {
     if (req.cookies.token) {
         const token = req.cookies.token;
-        const bool = jwt.verify(token, process.env.JWT_SECRET, (err, result) => {
+        const bool = jwt.verify(token, process.env.SECRET_JWT_TOKEN, (err, result) => {
             if (err) return false
             req.token = result
             return true
