5c7e8b71d14f87c6fcf377ef296ff5dab42a1a1d
MartinasR <martinasrudis@gmail.com>
2022-05-24 15:19:26 +0300

    register update

--- routes/ui/register.js
+++ routes/ui/register.js
@@ -1,12 +1,15 @@
 import express from "express";
-import con from '../../SQL_connection';
+import con from '../../SQL_connection.js';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import isAuthed from "../isAuthed.js";
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
     try {
-        res.render('register', { css: 'index.css' })
+        const auth = await isAuthed(req)
+        res.render('register', { css: 'index.css', isAuthed: auth })
 
     } catch (err) {
         res.json({ err: err });
@@ -14,17 +17,20 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
+    console.log(req.body)
     try {
-        const { username, password, repeatPassword } = req.body;
-        if (password !== repeatPassword) return res.json({ err: 'Passwords do not match' })
-        const [usernameCheck] = await con.query(`SELECT name FROM user WHERE name = ?`, [username])
-        if (usernameCheck.length > 0) return res.json({ err: 'Username already taken' })
+        const { name, email, password, repeatPassword } = req.body;
+        if (password !== repeatPassword) return res.json({ err: 'Pass not match' })
+        const [nameCheck] = await con.query(`SELECT name FROM user WHERE name = ?`, [name])
+        if (nameCheck.length > 0) return res.json({ err: 'Username exist ' })
         const hashedPassword = await bcrypt.hash(password, 10)
-        await con.query(`
-        INSERT INTO user (name, password, register_time)
-        VALUES(?, ?, ?)
-        `, [username, hashedPassword, new Date().toLocaleString('LT')]);
-        res.redirect('/')
+        const registeredUser = await con.query(`
+        INSERT INTO user (name, email, password, register_time)
+        VALUES(?, ?, ?, ?)
+        `, [name, email, hashedPassword, new Date().toLocaleString('LT')]);
+        const token = jwt.sign({ name: name, id: registeredUser[0].insertId }, process.env.SECRET_JWT_TOKEN, { expiresIn: '10m' })
+        res.cookie('token', token, { maxAge: 100000, httpOnly: true })
+            .redirect('/')
     } catch (err) {
         res.json({ err: err })
     }
